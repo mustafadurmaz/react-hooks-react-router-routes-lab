7780ac3bf8acf042bfe5880a8ae3f294067b85f6
Mustafa Durmaz <mdmz_001@hotmail.com>
2022-02-13 17:46:23 +0300

    commit tree 137667a8c93aeae8b296f7df475d12f2cbbf1acd

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorslist = actors.map((actor) => (
+    <div key={actor.name}>
+      <p>{actor.name}</p>
+
+      <ul>
+        {actor.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorslist}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorslist=directors.map((director)=>(
+    <div key={director.name}>
+      <p>{director.name}</p>
+      <ul>
+        {director.movies.map((movie)=>(
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return(
+    <div>
+      <h1>Directors Page</h1>
+      {directorslist}
+    </div>
+  )
 }
 
 export default Directors;
